03bacea05c701b7cb2e64386dea267e102b92a1b
unknown <emersincordoba2020@gmail.com>
2022-02-25 22:03:01 -0500

    ejercicios modulo 6 listos

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,23 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var Mayus = nombre[0].toUpperCase();
+  var Minus = nombre.slice(1);
+  var result = Mayus + Minus;
+  return result; 
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +28,19 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var result =numeros.reduce((acomulador, elemento) =>{
+    return acomulador + elemento;
+  },0);
+  return cb(result);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach((e) =>{
+    return cb(e);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +48,23 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  
+  const nuevoArray = array.map((e) => {
+    return cb(e);
+  })
+  return(nuevoArray);
 }
-
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var array2 = [];
+  array.map((e) =>{
+    if(e[0] === 'a'){
+      array2.push(e);
+    }
+  });
+  return array2;
 }
 
 // No modificar nada debajo de esta línea
